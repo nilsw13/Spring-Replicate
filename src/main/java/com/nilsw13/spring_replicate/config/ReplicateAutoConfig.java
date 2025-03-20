dddbd11f0f883d945920084e8f4d0e11553b4695
nilsw13 <nils.wenting@laplateforme.io>
2025-03-20 02:07:53 +0100

    feat: add ReplicateAutoConfig for auto-configuration of Replicate services
    - Added @Configuration class to enable auto-configuration for Replicate API integration.
    - Configured WebClient.Builder as a bean for customizable WebClient instances.
    - Implemented beans for ReplicateRestClient, AccountService, SecretSigningWebhookService, and Replicate.
    - Used @ConditionalOnProperty to ensure configuration is only active when replicate.api-key is present.
    - Added @ConditionalOnMissingBean to allow overriding of default beans.

---
+++ src/main/java/com/nilsw13/spring_replicate/config/ReplicateAutoConfig.java
@@ -0,0 +1,54 @@
+package com.nilsw13.spring_replicate.config;
+
+import com.nilsw13.spring_replicate.Replicate;
+import com.nilsw13.spring_replicate.api.ReplicateRestClient;
+import com.nilsw13.spring_replicate.impl.AccountServiceImpl;
+import com.nilsw13.spring_replicate.impl.SecretSigningWebhookServiceImpl;
+import com.nilsw13.spring_replicate.service.actions.AccountService;
+import com.nilsw13.spring_replicate.service.actions.ReplicateService;
+import com.nilsw13.spring_replicate.service.actions.SecretSigningWebhookService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@EnableConfigurationProperties(ReplicateProperties.class)
+@ConditionalOnProperty(prefix = "replicate", name = "api-key")
+public class ReplicateAutoConfig {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebClient.Builder webClientBuilder() {
+        return  WebClient.builder();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ReplicateRestClient replicateRestClient(ReplicateProperties replicateProperties) {
+        return  new ReplicateRestClient(replicateProperties);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AccountService accountService(ReplicateRestClient replicateRestClient) {
+        return new AccountServiceImpl(replicateRestClient);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecretSigningWebhookService secretSigningWebhookService(ReplicateRestClient replicateRestClient) {
+        return new SecretSigningWebhookServiceImpl(replicateRestClient);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Replicate replicate(AccountService accountService, SecretSigningWebhookService secretSigningWebhookService) {
+        return new ReplicateService(accountService, secretSigningWebhookService);
+    }
+
+
+
+}
