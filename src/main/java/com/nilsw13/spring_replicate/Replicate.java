549d21d8f17653b988c64950367cc9cddc47910d
nilsw13 <nils.wenting@laplateforme.io>
2025-03-20 02:04:14 +0100

    feat: add Replicate interface with account and webhook service methods

--- src/main/java/com/nilsw13/spring_replicate/Replicate.java
+++ src/main/java/com/nilsw13/spring_replicate/Replicate.java
@@ -1,4 +1,13 @@
 package com.nilsw13.spring_replicate;
 
+import com.nilsw13.spring_replicate.service.actions.AccountService;
+import com.nilsw13.spring_replicate.service.actions.SecretSigningWebhookService;
+
 public interface Replicate {
+
+    AccountService account();
+    SecretSigningWebhookService defaultSecretWebhook();
+
+
+
 }
