f35e16255d5b3a145df5d714d4fdd8238f0eebd3
nilsw13 <nils.wenting@laplateforme.io>
2025-03-20 02:12:54 +0100

    feat: add AccountServiceImpl for managing account-related operations
    - Implemented AccountService interface to handle account details retrieval.
    - Injected ReplicateRestClient for making API calls.
    - Provided method getAccountDetails to fetch account information from the Replicate API.

---
+++ src/main/java/com/nilsw13/spring_replicate/impl/AccountServiceImpl.java
@@ -0,0 +1,26 @@
+package com.nilsw13.spring_replicate.impl;
+
+import com.nilsw13.spring_replicate.api.ReplicateRestClient;
+import com.nilsw13.spring_replicate.model.AccountResponse;
+import com.nilsw13.spring_replicate.service.actions.AccountService;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+
+
+    private final ReplicateRestClient restClient;
+
+    public AccountServiceImpl(ReplicateRestClient restClient) {
+        this.restClient = restClient;
+    }
+
+
+    @Override
+    public Mono<AccountResponse> getAccountDetails() {
+        return restClient.get("account", AccountResponse.class);
+    }
+
+
+}
