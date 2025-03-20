2cb562f062ea3477898ca8aefec5e0752fba16d4
nilsw13 <nils.wenting@laplateforme.io>
2025-03-20 01:34:59 +0100

    first commit

---
+++ src/test/java/com/nilsw13/spring_replicate/SpringReplicateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nilsw13.spring_replicate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReplicateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
