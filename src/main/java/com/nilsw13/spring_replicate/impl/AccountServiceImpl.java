80e9855e6b9eea58ad4514a7100f61bd2e929dad
nilsw13 <nils.wenting@laplateforme.io>
2025-03-25 23:56:13 +0100

    add docs..

--- src/main/java/com/nilsw13/spring_replicate/impl/AccountServiceImpl.java
+++ src/main/java/com/nilsw13/spring_replicate/impl/AccountServiceImpl.java
@@ -5,6 +5,24 @@ import com.nilsw13.spring_replicate.api.ReplicateRestClient;
 import com.nilsw13.spring_replicate.service.AccountService;
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementation of the AccountService interface that communicates with the Replicate API.
+ *
+ * This service implementation retrieves account information by making HTTP requests
+ * to the Replicate API endpoint through the ReplicateRestClient. It provides access
+ * to the authenticated user's account details
+ *
+ * This class is annotated with @Service, making it eligible for Spring's component
+ * scanning and automatic dependency injection when included in a Spring application
+ * context.
+ *
+ * @author Nilsw13
+ * @since 1.0.0
+ * @see AccountService
+ * @see Account
+ * @see ReplicateRestClient
+ */
+
 @Service
 public class AccountServiceImpl implements AccountService {
 
--- src/main/java/com/nilsw13/spring_replicate/service/AccountService.java
+++ src/main/java/com/nilsw13/spring_replicate/service/AccountService.java
@@ -1,6 +1,22 @@
 package com.nilsw13.spring_replicate.service;
-
 import com.nilsw13.spring_replicate.ResponseType.Account.Account;
+
+
+/**
+ * Interface for accessing account information from the Replicate API.
+ *
+ * This service provides methods to retrieve details about the authenticated user's
+ * Replicate account
+ *
+ * Usage example:
+ *
+ * Account account = replicate.account().get();
+ * System.out.println("Username: " + account.getUsername());
+ * System.out.println("API Quota: " + account.getApiQuota());
+ *
+ * @author Nilsw13
+ * @since 1.0.0
+ */
 
 public interface AccountService {
 
